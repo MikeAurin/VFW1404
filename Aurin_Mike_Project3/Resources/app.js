8a7abe6d075e1728d0035c0519c523a074e95ac6
Mike Aurin <MikeAurin@Mikes-Mac.local>
2014-04-21 16:26:15 -0400

    Need to open images in next window

--- Aurin_Mike_Project3/Resources/app.js
+++ Aurin_Mike_Project3/Resources/app.js
@@ -1,82 +1,131 @@
-Ti.UI.setBackgroundColor("#000");
-
-var bands = {
-	"sevenfold": {
-		"headTitle": "Avenged Sevenfold",
-		"footTitle": "Orange County Metal Band",
-		"members": [
-			{
-				"name": "M. Shadows", 
-				"info": "Lead Singer.  Known for his raspy yet powerful vocals"
-			}, 
-			{
-				"name": "Synyster Gates", 
-				"info": "Lead guitarist and backup vocalist.  Known for technical solos and melodic riffs."
-			}, 
-			{
-				"name": "Zacky Vengeance",
-				"info": "Rhythm guitarist and backup vocalist.  Capable of tight and fast riffs and harmonizing with lead guitar parts. "
-			}, 
-			{
-				"name": "Johnny Christ",
-				"info": "Bassist.  Contributes heavy grooves as well as intricate undertones to the rhythm section."
-			}, 
-			{
-				"name": "Arin Iljey", 
-				"info": "Drummer.  New-comer to the band.  Filled in for the late Jimmy 'The Rev' Sullivan."
-			}, 
-		]
-	},
-	"adtr": {
-		"headTitle": "A Day To Remember",
-		"footTitle": "Ocala Based Pop-Punk Band",
-		"members":	[
-			{
-				"name": "Jeremey McKinnon", 
-				"info": "Lead Singer.  Capable of hitting clean high notes as well as low screams."
-			}, 
-			{
-				"name": "Neil Westfall",
-				"info": "Rhythm guitarist and backup vocalist.  Plays fast punk inspired rhythm sections while contributing back up screams during live shows."
-			},
-			{
-				"name": "Joshua Woodard",
-				"info": "Bassist.  Contributes to the heavy tones of the rhythm section."
-			},
-			{
-				"name": "Kevin Skaff", 
-				"info": "Lead guitarist and backup vocalist.  Plays the melodic section of songs and aids Jeremy with clean vocals both in the studio and on live shows."
-			},
-			{
-				"name": "Alex Shelnut", 
-				"info": "Drummer.  Adds fast punk fills with metal influenced double bass beats."
-			}
-		]
-	}
-};
-
+/*
 var mainWindow = Ti.UI.createWindow({
-	backgroundColor:"#fff"
+	title: "Mike's First Gallery",
+	backgroundImage: ("iphonebg.jpg")
 });
-var tableView = Ti.UI.createTableView({
-	top: 20
+
+var navWindow = Ti.UI.iOS.createNavigationWindow({
+	window: mainWindow
 });
-var makeUI = function(){
-for(var i in bands){
-		var bandsTable = Ti.UI.createTableViewSection({
-			text: bands[i].headTitle
-		});
-		for(var j = 0, k = bands[i].members.length; j < k; k++){
-			var bandsTableRow = Ti.UI.createTableViewRow({
-				text: bands[i].members.length
-			});
-		}
-		
-	};
-		
+
+var openGallery = function(){
+	var pWidth = Ti.Platform.displayCaps.platformWidth;
+	var pHeight = Ti.Platform.displayCaps.platformHeight;
+	var itemCount = 15;
+	var rowCount = 3;
+	var margin = 10;
+	var size = 40;
+	var mainGallery = Ti.UI.createWindow({
+		title: "Gallery",
+		backgroundImage: "iphonebg.jpg",
+	});
+	navWindow.openWindow(mainGallery);
+	var container = Ti.UI.createView({
+		width: pWidth,
+		backgroundcolor: "#fef"
+	});
+	navWindow.open(container);
 };
 
-makeUI();
+var titleImage = Ti.UI.createView({
+	backgroundImage: "cover.jpg",
+	borderRadius: 8,
+	top: 20,
+	left: 30,
+	right: 30,
+	bottom: 120
+});
 
-mainWindow.add(tableView);
-mainWindow.open();
+var beginButton = Ti.UI.createLabel({
+	height: 40,
+	bottom: 30,
+	left: 30,
+	right: 30,
+	height: 50,
+	borderRadius: 10,
+	backgroundColor: "#c42332",
+	text: "Start Gallery",
+	font: {fontFamily: "Verdana", fontSize: 30, fontWeight: "bold"},
+	color: "#fff",
+	textAlign: "center"
+});
+
+var getImages = require("gallery");
+
+beginButton.addEventListener("click", openGallery);
+
+mainWindow.add(beginButton);
+mainWindow.add(titleImage);
+navWindow.open();
+*/
+
+var pWidth = Ti.Platform.displayCaps.platformWidth;
+var pHeight = Ti.Platform.displayCaps.platformHeight;
+var rowCount = 3;
+var margin = 10;
+var trueCanvasWidth = (pWidth - (margin * 2)) - rowCount;
+var size = ((pHeight - trueCanvasWidth)/rowCount);
+var imagesFolder = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "proj3images");
+var imageFiles = imagesFolder.getDirectoryListing();
+
+console.log(imageFiles);
+
+var mainWin = Ti.UI.createWindow({
+	backgroundColor: "#fff",
+	layout: "horizontal",
+	title: "Images"
+});
+
+var navWindow = Ti.UI.iOS.createNavigationWindow({
+	window: mainWin,
+	top: 0
+});
+
+var border = Ti.UI.createView({
+	backgroundColor: "#cecece",
+	height: 1,
+	top: 0
+});
+
+viewContainer = Ti.UI.createScrollView({
+	top: 0,
+	layout: "horizontal",
+	width: pWidth,
+	height: pHeight-border.top-border.height,
+	contentWidth: pWidth,
+	showVerticalScrollIndicator: true,
+	backgroundColor: "black"
+});
+
+var getImage = function(){
+	var imageWindow = Ti.UI.createWindow({
+		backgroundColor: "#fff",
+		image: "proj3images/" + imageFiles[i]
+	});
+	
+	imageWindow.open();
+};
+
+for(var i=0; i<imageFiles.length; i++){
+	var view = Ti.UI.createView({
+		backgroundColor: "black",
+		top: 10,
+		left: 10,
+		width: size,
+		height: size
+	});
+	var thumb = Ti.UI.createImageView({
+		image: "proj3images/" + imageFiles[i],
+		top: 0,
+		width: view.width*2
+	});
+	
+	view.add(thumb);
+	viewContainer.add(view);
+	thumb.addEventListener("click", getImage); 
+}
+
+mainWin.add(border);
+mainWin.add(viewContainer);
+mainWin.open();
+navWindow.open(mainWin);
