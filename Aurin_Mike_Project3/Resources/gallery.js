d4d6d761e3ac764c78a67bd8fa9fd93cd8f3b67a
Mike Aurin <MikeAurin@Mikes-Mac.local>
2014-04-28 10:05:25 -0400

    Converted application into a Nav View

--- Aurin_Mike_Project3/Resources/gallery.js
+++ Aurin_Mike_Project3/Resources/gallery.js
@@ -1,32 +1,68 @@
+var pWidth = Ti.Platform.displayCaps.platformWidth;
+var pHeight = Ti.Platform.displayCaps.platformHeight;
+var rowCount = 3;
+var margin = 10;
+var trueCanvasWidth = (pWidth - (margin * 2)) - rowCount;
+var size = ((pHeight - trueCanvasWidth)/rowCount);
 var imagesFolder = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "proj3images");
 var imageFiles = imagesFolder.getDirectoryListing();
 
 console.log(imageFiles);
 
 var mainWin = Ti.UI.createWindow({
-	backgroundColor: "#fff",
-	layout: "horizontal"
+	layout: "horizontal",
+	title: "Image Gallery",
+});
+
+var navWindow = Ti.UI.iOS.createNavigationWindow({
+	window: mainWin
 });
 
 var border = Ti.UI.createView({
 	backgroundColor: "#cecece",
-	height: 1,
-	top: 20
+	height: 20,
+	top: 0
 });
 
-viewContainer = Ti.UI.createScrollView({
+var viewContainer = Ti.UI.createScrollView({
 	top: 0,
 	layout: "horizontal",
 	width: pWidth,
-	height: pHeight-border.top-border.height,
+	height: pHeight-border.height-border.top,
 	contentWidth: pWidth,
 	showVerticalScrollIndicator: true,
-	backgroundColor: "#fef"
+	backgroundColor: "#cecece"
 });
+
+mainWin.add(border, viewContainer);
+
+var backButton = Ti.UI.createLabel({
+	top: 0,
+	height: 50,
+	width: "100%",
+	backgroundColor: "grey",
+	text: "Close Gallery",
+	color: "white",
+	textAlign: "center"
+});
+
+mainWin.add(backButton);
+
+var getImage = function(){
+	var imageWindow = Ti.UI.createImageView({
+		backgroundColor: "#cecece",
+		image: this.image
+	});
+
+	viewContainer.add(imageWindow);
+	imageWindow.open();
+	
+
+};
 
 for(var i=0; i<imageFiles.length; i++){
 	var view = Ti.UI.createView({
-		backgroundColor: "#33CCFF",
+		backgroundColor: "#cecece",
 		top: 10,
 		left: 10,
 		width: size,
@@ -34,13 +70,13 @@ for(var i=0; i<imageFiles.length; i++){
 	});
 	var thumb = Ti.UI.createImageView({
 		image: "proj3images/" + imageFiles[i],
-		top: 0,
 		width: view.width*2
 	});
-	
+
 	view.add(thumb);
 	viewContainer.add(view);
+	thumb.addEventListener("click", getImage); 
 }
- mainWin.add(border);
- mainWin.add(viewContainer);
- mainWin.open();
+
+navWindow.add(mainWin);
+navWindow.open();
